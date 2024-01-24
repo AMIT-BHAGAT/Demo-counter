d6445287d4126fdfac3141cc477ceed9467b4c32
AMIT-BHAGAT <bhagatamit94@gmail.com>
2024-01-25 01:04:08 +0530

    adf

--- src/main/java/com/example/springboot/SpringbootApplication.java
+++ src/main/java/com/example/springboot/SpringbootApplication.java
@@ -4,7 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
 public class SpringbootApplication {
+
+	@GetMapping
+	public String message() {
+		return "welcome to Mr. DevOps Youtube Channel";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
