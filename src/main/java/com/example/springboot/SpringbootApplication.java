f8a26af65d8c853b09f5baad373bfb0b01da34e4
AMIT-BHAGAT <bhagatamit94@gmail.com>
2024-01-25 13:38:29 +0530

    asdf

--- src/main/java/com/example/springboot/SpringbootApplication.java
+++ src/main/java/com/example/springboot/SpringbootApplication.java
@@ -2,17 +2,17 @@ package com.example.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+// @RestController
 public class SpringbootApplication {
 
-	@GetMapping
-	public String message() {
-		return "welcome to Mr. DevOps Youtube Channel";
-	}
+	// @GetMapping
+	// public String message() {
+	// return "welcome to Mr. DevOps Youtube Channel";
+	// }
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
